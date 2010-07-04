f4e5ba86e4000bbb53f0aa0e37f00d1e24a0bb0f
Florian Bertholin <florian.bertholin@gmail.com>
2010-07-04 22:23:55 +0200

    First release of AkText

---
+++ lib/ak_text.rb
@@ -0,0 +1,34 @@
+# AkText
+
+module AkText
+  def levenshtein_with(s2)
+	self.levenshtein(self, s2)
+  end
+  
+  def self.levenshtein(str1, str2)
+    if $KCODE =~ /^U/i
+      unpack_rule = 'U*'
+    else
+      unpack_rule = 'C*'
+    end
+    s = str1.unpack(unpack_rule)
+    t = str2.unpack(unpack_rule)
+    n = s.length
+    m = t.length
+    return m if (0 == n)
+    return n if (0 == m)
+    d = (0..m).to_a
+    x = nil
+    (0...n).each do |i|
+      e = i + 1
+      (0...m).each do |j|
+        cost = (s[i] == t[j]) ? 0 : 1
+        x = [d[j+1] + 1, e + 1, d[j] + cost].min
+        d[j] = e
+        e = x
+      end
+      d[m] = x
+    end
+    return x
+  end
+end
